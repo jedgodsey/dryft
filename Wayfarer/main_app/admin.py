6378f80f0ae58dcc1198ea3f4efc185b36fb6114
jedgodsey <jed.godsey@gmail.com>
2020-10-30 15:09:06 -0700

    created models and tested data

--- Wayfarer/main_app/admin.py
+++ Wayfarer/main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, City, Post
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(City)
--- Wayfarer/main_app/models.py
+++ Wayfarer/main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length = 100)
+    country = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length = 100)
+    current_city = models.ForeignKey(City, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length = 250)
+    content = models.TextField(max_length = 1000)
+    date = models.DateField()
+    profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.title
