bce03849c06238b69ccae322793face49d909718
Bryanromerou <Bryanromerou@gmail.com>
2020-11-01 10:39:03 -0800

    added picture property to all of our models

--- Wayfarer/main_app/models.py
+++ Wayfarer/main_app/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class City(models.Model):
     name = models.CharField(max_length = 100)
     country = models.CharField(max_length = 100)
+    image = models.ImageField(upload_to='images/city_pics/', null=True)
     def __str__(self):
         return self.name
 
@@ -14,6 +15,7 @@ class Profile(models.Model):
     name = models.CharField(max_length = 100)
     current_city = models.ForeignKey(City, on_delete = models.CASCADE, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='images/profile_pics/', default = "images/profile_pics/profile_placeholder.jpg")
 
     def __str__(self):
         return self.name
@@ -24,5 +26,6 @@ class Post(models.Model):
     content = models.TextField(max_length = 1000)
     date = models.DateField()
     profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
+    image = models.ImageField(upload_to='images/post_pics/', null = True)
     def __str__(self):
         return self.title
--- Wayfarer/wayfarer_project/settings.py
+++ Wayfarer/wayfarer_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
