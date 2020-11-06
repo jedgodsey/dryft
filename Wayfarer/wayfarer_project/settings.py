4fd4191e4dac838a99bb2b3e0c70a2697732b9a0
jedgodsey <jed.godsey@gmail.com>
2020-11-06 10:13:42 -0800

    adds heroku deployment config

--- Wayfarer/wayfarer_project/settings.py
+++ Wayfarer/wayfarer_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +128,5 @@ LOGOUT_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_on_heroku.settings(locals())
