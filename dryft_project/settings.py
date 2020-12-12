67a9fa39b9821e81d3e547349aa9e51b13efc479
jedgodsey <jed.godsey@gmail.com>
2020-12-12 12:08:39 -0800

    committing some name changes

--- dryft_project/settings.py
+++ dryft_project/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'wayfarer_project.urls'
+ROOT_URLCONF = 'dryft_project.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wayfarer_project.wsgi.application'
+WSGI_APPLICATION = 'dryft_project.wsgi.application'
 AUTH_PROFILE_MODULE = 'accounts.Profile'
 
 # Database
